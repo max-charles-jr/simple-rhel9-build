2d0e888c5bb01c725cd2ff4108afd9a54b167988
Max Charles Jr. <mcharles1682@gmail.com>
2025-02-07 11:28:52 -0500

    Fixed path module for ZIP file

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,12 +1,12 @@
 # Lambda Function
 resource "aws_lambda_function" "stop_ec2_lambda" {
-  filename      = "lambda_function_payload.zip" # Replace with your Lambda deployment package
+  filename      = "${path.module}/lambda_function.zip" # Replace with your Lambda deployment package
   function_name = "stop_ec2_instance"
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
 
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+  source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")
 
   environment {
     variables = {
